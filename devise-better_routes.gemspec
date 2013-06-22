e9179fc0b4b4e56dd02ce2617ad20203c110c793
Toru KAWAMURA <tkawa@4bit.net>
2013-06-22 16:16:39 +0900

    Add a patch of basic registration (Devise Registerable module)

--- devise-better_routes.gemspec
+++ devise-better_routes.gemspec
@@ -4,21 +4,22 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'devise/better_routes/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "devise-better_routes"
+  spec.name          = 'devise-better_routes'
   spec.version       = Devise::BetterRoutes::VERSION
-  spec.authors       = ["Toru KAWAMURA"]
-  spec.email         = ["tkawa@4bit.net"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+  spec.authors       = ['Toru KAWAMURA']
+  spec.email         = ['tkawa@4bit.net']
+  spec.description   = %q{Better routes patch for Devise}
+  spec.summary       = %q{Better routes patch for Devise}
+  spec.homepage      = ''
+  spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec"
+  spec.add_dependency 'devise', '>= 3.0.0.rc'
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec'
 end
--- lib/devise/better_routes.rb
+++ lib/devise/better_routes.rb
@@ -1,7 +1,60 @@
 require "devise/better_routes/version"
 
 module Devise
-  module BetterRoutes
-    # Your code goes here...
+  module Controllers
+    module UrlHelpers
+      def self.override_registration_helpers!(registration_routes = nil)
+        helper_mappings = {
+          registration: '#{scope.to_s.pluralize}',
+          new_registration: 'new_#{scope}',
+          edit_registration: 'edit_current_#{scope}',
+          cancel_registration: 'cancel_current_#{scope}'
+        }
+        [:path, :url].each do |path_or_url|
+          helper_mappings.each do |old_name, new_name|
+            method = "#{old_name}_#{path_or_url}"
+            class_eval <<-URL_HELPERS, __FILE__, __LINE__ + 1
+              def #{method}(resource_or_scope, *args)
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                send("#{new_name}_#{path_or_url}", *args)
+              end
+            URL_HELPERS
+          end
+        end
+      end
+      override_registration_helpers!(Devise::URL_HELPERS[:registration])
+
+      def self.run_generate_hooks!
+        self.override_registration_helpers!(Devise::URL_HELPERS[:registration])
+      end
+    end
   end
+
+  def self.regenerate_helpers!
+    Devise::Controllers::UrlHelpers.remove_helpers!
+    Devise::Controllers::UrlHelpers.generate_helpers!
+    Devise::Controllers::UrlHelpers.run_generate_hooks!
+  end
+end
+
+module ActionDispatch
+  module Routing
+    class Mapper
+      protected
+        def devise_registration(mapping, controllers)
+          path, as, @scope[:path], @scope[:as] = @scope[:path], @scope[:as], nil, nil
+          current_resource_name = mapping.path_names["current_#{mapping.singular}".to_sym]
+          resources mapping.path, only: [:create, :new]
+          resource current_resource_name, only: [:edit, :update, :destroy] do
+            get :cancel
+          end
+        ensure
+          @scope[:path], @scope[:as] = path, as
+        end
+    end
+  end
+end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.uncountable %w( my me )
 end
