b4a91c3bd27b663b99717cb7221061c1b56f4c8c
Toru KAWAMURA <tkawa@4bit.net>
2013-06-23 13:10:11 +0900

    Rewrite specs using "expect"

--- spec/devise/better_routes_spec.rb
+++ spec/devise/better_routes_spec.rb
@@ -1,97 +1,98 @@
 require 'spec_helper'
 
 describe Devise::BetterRoutes do
-  it 'should have a version number' do
-    Devise::BetterRoutes::VERSION.should_not be_nil
+  it 'expect to have a version number' do
+    expect(Devise::BetterRoutes::VERSION).not_to be_nil
   end
 
-  describe 'routing users' do
+  context 'routing' do
     include RSpec::Rails::RoutingExampleGroup
-    it 'routes to users#create' do
-      post('/users').should route_to('users#create')
+    describe 'users' do
+      it 'routes to users#create' do
+        expect(post('/users')).to route_to('users#create')
+      end
+      it 'routes to users#new' do
+        expect(get('/users/new')).to route_to('users#new')
+      end
+      it 'routes to current_users#cancel' do
+        expect(get('/current_user/cancel')).to route_to('current_users#cancel')
+      end
+      it 'routes to current_users#edit' do
+        expect(get('/current_user/edit')).to route_to('current_users#edit')
+      end
+      it 'routes to current_users#update' do
+        expect(put('/current_user')).to route_to('current_users#update')
+        #expect(patch('/current_user')).to route_to('current_users#update')
+      end
+      it 'routes to current_users#destroy' do
+        expect(delete('/current_user')).to route_to('current_users#destroy')
+      end
     end
-    it 'routes to users#new' do
-      get('/users/new').should route_to('users#new')
-    end
-    it 'routes to current_users#cancel' do
-      get('/current_user/cancel').should route_to('current_users#cancel')
-    end
-    it 'routes to current_users#edit' do
-      get('/current_user/edit').should route_to('current_users#edit')
-    end
-    it 'routes to current_users#update' do
-      put('/current_user').should route_to('current_users#update')
-      #patch('/current_user').should route_to('current_users#update')
-    end
-    it 'routes to current_users#destroy' do
-      delete('/current_user').should route_to('current_users#destroy')
+
+    describe 'rails_programmers' do
+      it 'routes to rails_programmers#create' do
+        expect(post('/rails_programmers')).to route_to('rails_programmers#create')
+      end
+      it 'routes to rails_programmers#new' do
+        expect(get('/rails_programmers/new')).to route_to('rails_programmers#new')
+      end
+      it 'routes to current_rails_programmers#cancel' do
+        expect(get('/current_rails_programmer/cancel')).to route_to('current_rails_programmers#cancel')
+      end
+      it 'routes to current_rails_programmers#edit' do
+        expect(get('/current_rails_programmer/edit')).to route_to('current_rails_programmers#edit')
+      end
+      it 'routes to current_rails_programmers#update' do
+        expect(put('/current_rails_programmer')).to route_to('current_rails_programmers#update')
+        #expect(patch('/current_rails_programmer')).to route_to('current_rails_programmers#update')
+      end
+      it 'routes to current_rails_programmers#destroy' do
+        expect(delete('/current_rails_programmer')).to route_to('current_rails_programmers#destroy')
+      end
     end
   end
 
-  describe 'routing rails_programmers' do
-    include RSpec::Rails::RoutingExampleGroup
-    it 'routes to rails_programmers#create' do
-      post('/rails_programmers').should route_to('rails_programmers#create')
-    end
-    it 'routes to rails_programmers#new' do
-      get('/rails_programmers/new').should route_to('rails_programmers#new')
-    end
-    it 'routes to current_rails_programmers#cancel' do
-      get('/current_rails_programmer/cancel').should route_to('current_rails_programmers#cancel')
-    end
-    it 'routes to current_rails_programmers#edit' do
-      get('/current_rails_programmer/edit').should route_to('current_rails_programmers#edit')
-    end
-    it 'routes to current_rails_programmers#update' do
-      put('/current_rails_programmer').should route_to('current_rails_programmers#update')
-      #patch('/current_rails_programmer').should route_to('current_rails_programmers#update')
-    end
-    it 'routes to current_rails_programmers#destroy' do
-      delete('/current_rails_programmer').should route_to('current_rails_programmers#destroy')
-    end
-  end
-
-  describe 'URL helpers for users' do
+  context 'URL helpers' do
     include Rails.application.routes.url_helpers
     include Devise::Controllers::UrlHelpers
-    it { users_path.should eq '/users' }
-    it { new_user_path.should eq '/users/new' }
-    it { cancel_current_user_path.should eq '/current_user/cancel' }
-    it { edit_current_user_path.should eq '/current_user/edit' }
-    it { current_user_path.should eq '/current_user' }
-    it 'registration_path is delegated to users_path' do
-      registration_path(:user).should eq users_path
+    describe 'users' do
+      specify { expect(users_path).to eq '/users' }
+      specify { expect(new_user_path).to eq '/users/new' }
+      specify { expect(cancel_current_user_path).to eq '/current_user/cancel' }
+      specify { expect(edit_current_user_path).to eq '/current_user/edit' }
+      specify { expect(current_user_path).to eq '/current_user' }
+      it 'registration_path is delegated to users_path' do
+        expect(registration_path(:user)).to eq users_path
+      end
+      it 'new_registration_path is delegated to new_user_path' do
+        expect(new_registration_path(:user)).to eq new_user_path
+      end
+      it 'cancel_registration_path is delegated to cancel_current_user_path' do
+        expect(cancel_registration_path(:user)).to eq cancel_current_user_path
+      end
+      it 'edit_registration_path is delegated to edit_current_user_path' do
+        expect(edit_registration_path(:user)).to eq edit_current_user_path
+      end
     end
-    it 'new_registration_path is delegated to new_user_path' do
-      new_registration_path(:user).should eq new_user_path
-    end
-    it 'cancel_registration_path is delegated to cancel_current_user_path' do
-      cancel_registration_path(:user).should eq cancel_current_user_path
-    end
-    it 'edit_registration_path is delegated to edit_current_user_path' do
-      edit_registration_path(:user).should eq edit_current_user_path
-    end
-  end
 
-  describe 'URL helpers for rails_programmers' do
-    include Rails.application.routes.url_helpers
-    include Devise::Controllers::UrlHelpers
-    it { rails_programmers_path.should eq '/rails_programmers' }
-    it { new_rails_programmer_path.should eq '/rails_programmers/new' }
-    it { cancel_current_rails_programmer_path.should eq '/current_rails_programmer/cancel' }
-    it { edit_current_rails_programmer_path.should eq '/current_rails_programmer/edit' }
-    it { current_rails_programmer_path.should eq '/current_rails_programmer' }
-    it 'registration_path is delegated to rails_programmers_path' do
-      registration_path(:rails_programmer).should eq rails_programmers_path
-    end
-    it 'new_registration_path is delegated to new_rails_programmer_path' do
-      new_registration_path(:rails_programmer).should eq new_rails_programmer_path
-    end
-    it 'cancel_registration_path is delegated to cancel_current_rails_programmer_path' do
-      cancel_registration_path(:rails_programmer).should eq cancel_current_rails_programmer_path
-    end
-    it 'edit_registration_path is delegated to edit_current_rails_programmer_path' do
-      edit_registration_path(:rails_programmer).should eq edit_current_rails_programmer_path
+    describe 'rails_programmers' do
+      specify { expect(rails_programmers_path).to eq '/rails_programmers' }
+      specify { expect(new_rails_programmer_path).to eq '/rails_programmers/new' }
+      specify { expect(cancel_current_rails_programmer_path).to eq '/current_rails_programmer/cancel' }
+      specify { expect(edit_current_rails_programmer_path).to eq '/current_rails_programmer/edit' }
+      specify { expect(current_rails_programmer_path).to eq '/current_rails_programmer' }
+      it 'registration_path is delegated to rails_programmers_path' do
+        expect(registration_path(:rails_programmer)).to eq rails_programmers_path
+      end
+      it 'new_registration_path is delegated to new_rails_programmer_path' do
+        expect(new_registration_path(:rails_programmer)).to eq new_rails_programmer_path
+      end
+      it 'cancel_registration_path is delegated to cancel_current_rails_programmer_path' do
+        expect(cancel_registration_path(:rails_programmer)).to eq cancel_current_rails_programmer_path
+      end
+      it 'edit_registration_path is delegated to edit_current_rails_programmer_path' do
+        expect(edit_registration_path(:rails_programmer)).to eq edit_current_rails_programmer_path
+      end
     end
   end
 end
